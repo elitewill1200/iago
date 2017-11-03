8cfdca35c18c98c69b3cca368a7a2706ba2eed66
elitewill1200 <longsworthwill@yahoo.com>
2017-11-02 20:23:44 -0400

    Added neuralNet abstract

---
+++ neuralNet.java
@@ -0,0 +1,8 @@
+public class neuralNet{
+  public neuralNet(int inputs, int hiddenLayers, int neuronsPerLayer, int outputs){
+  }
+  private int[] activation(boolean[][]){
+  }
+  public int takeTurn(boolean[][]){
+  }
+}
